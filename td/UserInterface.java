6773fdcb38a92e0281701b2a23f96ce503150da7
Claquettes <math@claq.fr>
2023-12-07 10:56:37 +0100

    ask for known command until given

--- td/UserInterface.java
+++ td/UserInterface.java
@@ -9,16 +9,21 @@ public class UserInterface {
         System.out.println("Appuyez sur F pour lancer le modèle de feu");
 
         Scanner scanner = new Scanner(System.in);
+        String input;
 
-        if (scanner.nextLine().equals("F")) {
-            System.out.println("Lancement du modèle de feu");
-            System.out.print("\033[H\033[2J");  // Clear console
-            System.out.flush();  // Clear console
-            Grille grille = new Grille(2, 8, new EtatCellule("FEU"));
-            System.out.println(grille);
-            System.out.println(grille.getCellules());
-        } else {
-            System.out.println("Commande inconnue");
-        }
+        do {
+            input = scanner.nextLine();
+
+            if (input.equals("F")) {
+                System.out.println("Lancement du modèle de feu");
+                System.out.print("\033[H\033[2J");  // Clear console
+                System.out.flush();  // Clear console
+                Grille grille = new Grille(2, 8, new EtatCellule("FEU"));
+                System.out.println(grille);
+                System.out.println(grille.getCellules());
+            } else {
+                System.out.println("Commande inconnue, veuillez réessayer");
+            }
+        } while (!input.equals("F") && !input.equals("q"));
     }
 }
