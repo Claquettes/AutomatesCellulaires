184a0aa34e1204c35e3a079d5bc99f3d804dfc1f
Bex138 <152154753+Bex138@users.noreply.github.com>
2023-12-31 20:26:30 +0100

    ajout de localrule dans automate

--- td/Automate.java
+++ td/Automate.java
@@ -1,7 +1,9 @@
 package AutomatesCellulaires.td;
+import AutomatesCellulaires.td.LocalRule;
 
 public class Automate {
     public Grille grid;
+    Public LocalRule localRule;
     // public LocalRule lr; a rajouter quand la classe LocalRule sera créée
 
     // constructeur en mode prédéfini,
