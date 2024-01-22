127d41c1f6402854e313aee2447af40d97b42948
Claquettes <math@claq.fr>
2024-01-22 14:09:55 +0100

    Refactor code to improve performance and readability

--- td/Automate.java
+++ td/Automate.java
@@ -8,86 +8,46 @@ import java.util.Scanner;
 public class Automate {
     public Grille grid;
     public LocalRule localRule;
-    // public LocalRule lr; a rajouter quand la classe LocalRule sera créée
-    // nombre de voisins détérminé ( 1 0 1 -> cellule actuelle "1" et 2 voisines)
     public Integer nombreVoisins;
-
-    // etats possibles des cellules = Q, un ensemble fini, est son alphabet
     public EtatCellule etatCellules;
-
-    private Grille gridCopy; // qui va contenier la grille à l'état N pour pouvoir calculer l'etat N+1
+    private Grille gridCopy;
     private Integer nbCol = 10;
     private Integer nbLigne = 5;
 
-    // constructeur en mode prédéfini,
-    // NE PAS OUBLIER DE INITIALISER GRIDCOPY !!!!!
     public Automate(String name) {
         askForGridSize();
         if (name.toUpperCase().equals("FEU")) {
-            // predefini avec 4 voisins
             this.nombreVoisins = 4;
             EtatCellule etatFeu = new EtatCellule("FEU");
             this.etatCellules = etatFeu;
-            System.out.println("WE HAVE THIS ETATCELLULES : " + this.etatCellules);
             this.localRule = new LocalRule(etatFeu.getEtatChoisie(), this.nombreVoisins, "FEU");
-            // this.grid = new Grille(2, 8, new EtatCellule("FEU"));
-            System.out.println("Création de l'automate " + name + " en mode prédéfini");
-
-            //Grille grille = new Grille(2, 8, etatFeu);
-            //8
-            this.grid = new Grille(2,3,2, etatFeu);
+            this.grid = new Grille(2, 3, 2, etatFeu);
             this.gridCopy = new Grille(grid);
             System.out.println(grid);
-            System.out.print(gridCopy);
 
             for (int i = 0; i < 5; i++) {
                 miseAJour();
                 System.out.println(grid);
-                /*try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }*/
             }
-
-            //System.out.println(grid.getCellules());
-            // we quit the program
             System.exit(0);
-        }
-        if (name.equals("CONWAY")) {
-            // this.grid = new Grille(2, 8, new EtatCellule("CONWAY"));
-            System.out.println("Création de l'automate " + name + " en mode prédéfini");
         } else {
             System.out.println("Erreur : nom d'automate inconnu...");
         }
     }
 
-    // constructeur en mode 1D, donc custom vu qu'on passe le numero de la
-    // regle
     public Automate(String name, Integer nbVoisins, Integer ruleNumber) {
         askForGridSize();
         if (name.toUpperCase().equals("1D")) {
-            // predefini avec 4 voisins
             this.nombreVoisins = 1;
             EtatCellule etat1D = new EtatCellule("1D");
             this.etatCellules = etat1D;
             this.localRule = new LocalRule(etat1D.getEtatChoisie(), this.nombreVoisins, ruleNumber);
-            // this.grid = new Grille(2, 8, new EtatCellule("FEU"));
-            System.out.println("Création de l'automate " + name + " en mode prédéfini");
-            // Grille grille = new Grille(2, 8, etatFeu);
-            this.grid = new Grille(1, this.nbCol,1,  etat1D);
+            this.grid = new Grille(1, this.nbCol, 1, etat1D);
             this.gridCopy = new Grille(grid);
-
-            //System.out.println(grid);
-
             for (int i = 0; i < 5; i++) {
                 miseAJour();
                 System.out.println(grid);
             }
-
-            //System.out.print(gridCopy);
-            //System.out.println(grid.getCellules());
-            // we quit the program
             System.exit(0);
         } else {
             System.out.println("Erreur : nom d'automate inconnu...");
@@ -118,92 +78,90 @@ public class Automate {
         return etatSuivant;
     }
 
-
     /**
      * Trouve la configuration des voisins de la cellule
      * 
      * @param x coordonnee x de la cellule
      * @ruturn String qui contient la configuration des voisins
      */
-    private String getConfigVoisin(int x, int y){
+    private String getConfigVoisin(int x, int y) {
 
         String configVoisin = this.gridCopy.getValeurCellule(x, y) + ";";
 
         switch (this.nombreVoisins) {
             case 1:
-                
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";" ;
+
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";";
                 return configVoisin;
 
             case 2:
 
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";" 
-                + this.gridCopy.getValeurCellule(x, y-1) + ";";
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x, y - 1) + ";";
                 return configVoisin;
 
             case 3:
-                
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";" 
-                + this.gridCopy.getValeurCellule(x, y-1) + ";" 
-                + this.gridCopy.getValeurCellule(x+1, y)+ ";"  ;
+
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y) + ";";
                 return configVoisin;
 
             case 4:
 
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";" 
-                + this.gridCopy.getValeurCellule(x, y-1) + ";" 
-                + this.gridCopy.getValeurCellule(x+1, y) + ";" 
-                + this.gridCopy.getValeurCellule(x-1, y)+ ";"  ;
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y) + ";";
                 return configVoisin;
 
             case 5:
 
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";"
-                + this.gridCopy.getValeurCellule(x, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x+1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y-1)+ ";"  ;
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y - 1) + ";";
                 return configVoisin;
 
-            case 6: 
+            case 6:
 
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";"
-                + this.gridCopy.getValeurCellule(x, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x+1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y+1) + ";" ;
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y + 1) + ";";
                 return configVoisin;
 
             case 7:
 
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";"
-                + this.gridCopy.getValeurCellule(x, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x+1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y+1) + ";"
-                + this.gridCopy.getValeurCellule(x+1, y-1)+ ";"  ;
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y - 1) + ";";
                 return configVoisin;
 
             case 8:
 
-                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y+1) + ";"
-                + this.gridCopy.getValeurCellule(x, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x+1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x-1, y+1) + ";"
-                + this.gridCopy.getValeurCellule(x+1, y-1) + ";"
-                + this.gridCopy.getValeurCellule(x+1, y+1) + ";" ;
+                configVoisin = configVoisin + this.gridCopy.getValeurCellule(x, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x - 1, y + 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y - 1) + ";"
+                        + this.gridCopy.getValeurCellule(x + 1, y + 1) + ";";
                 return configVoisin;
-            
+
             default:
 
                 configVoisin = "NONE";
                 return configVoisin;
 
-            
         }
 
     }
@@ -216,14 +174,14 @@ public class Automate {
         // copie Grille gridCopie;
         this.gridCopy.copieEtatCellules(this.grid);
 
-        // pour parcouir la grille 
-        for(int i=0; i<this.gridCopy.nbLine; i++){
+        // pour parcouir la grille
+        for (int i = 0; i < this.gridCopy.nbLine; i++) {
 
-            for( int j = 0; j < this.gridCopy.nbCol; j++){
+            for (int j = 0; j < this.gridCopy.nbCol; j++) {
 
                 // recupere etat de la cellule et etat cellules voisines
                 String etatCellule = this.gridCopy.getValeurCellule(i, j);
-                String etatVoisines = getConfigVoisin(i,j);
+                String etatVoisines = getConfigVoisin(i, j);
 
                 // calcule l'etat suivant et le stoque dans la copie
                 String etatSuivant = getEtatSuivantCellule(etatVoisines);
@@ -240,30 +198,30 @@ public class Automate {
      * On demande a l'utilisateur de choisir le colonnes et de lignes de la grille
      */
 
-     public void askForGridSize(){
+    public void askForGridSize() {
         System.out.println("Voulez vous utiliser la taille par défault pour la grille (10*5) ? (Y/N)");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        if(input.equals("Y") || input.equals("y")){
+        if (input.equals("Y") || input.equals("y")) {
             this.nbCol = 10;
             this.nbLigne = 5;
-        }else{
+        } else {
 
             System.out.println("Veuillez entrer le nombre de colonnes de la grille");
             Scanner scannerNbCol = new Scanner(System.in);
             this.nbCol = scannerNbCol.nextInt();
-            if(nbCol < 1){
+            if (nbCol < 1) {
                 System.out.println("Le nombre de colonnes doit être supérieur à 0");
                 System.exit(0);
             }
             System.out.println("Veuillez entrer le nombre de lignes de la grille");
             Scanner scannerNbLigne = new Scanner(System.in);
-            if(nbLigne < 0){
+            if (nbLigne < 0) {
                 System.out.println("Le nombre de lignes doit être supérieur à 0");
                 System.exit(0);
             }
             this.nbLigne = scannerNbLigne.nextInt();
         }
-     }
+    }
 
 }
