f1b8cf8fd5b2498156b9ac4343fb6189b903a79e
Claquettes <math@claq.fr>
2024-01-22 14:51:08 +0100

    Add documentation for EtatCellule class

--- td/EtatCellule.java
+++ td/EtatCellule.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 
 import java.util.Map;
 
+/**
+ * This class represents the state of a Cell in an Automaton.
+ * It has a map of possible states and a chosen state.
+ */
+
 public class EtatCellule {
-    //etat "neutre" doit etre en premier
     private Map<String, ArrayList<String>> etats = Map.of(
             "1D", new ArrayList<>(Arrays.asList("0", "1")),
             "FEU", new ArrayList<>(Arrays.asList("VIDE", "ARBRE", "FEU", "CENDRE")),
@@ -14,6 +18,12 @@ public class EtatCellule {
 
     private ArrayList<String> etatChoisie;
 
+    /**
+     * Constructor for the EtatCellule class.
+     * It initializes the EtatCellule with a given type.
+     * 
+     * @param type The type of the EtatCellule.
+     */
     public EtatCellule(String type) {
         if (this.etats.containsKey(type)) {
             this.etatChoisie = this.etats.get(type);
@@ -23,10 +33,21 @@ public class EtatCellule {
         }
     }
 
+    /**
+     * Gets the chosen state of the EtatCellule.
+     * 
+     * @return The chosen state of the EtatCellule.
+     */
     public ArrayList<String> getEtatChoisie() {
         return this.etatChoisie;
     }
 
+    /**
+     * Gets the state of the EtatCellule by index.
+     * 
+     * @param index The index of the state.
+     * @return The state of the EtatCellule at the given index.
+     */
     public String getEtatByIndex(int index) {
         return this.etatChoisie.get(index);
     }
